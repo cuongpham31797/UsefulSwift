e238d39559b94a16068279fa2a771afd0f33fb5b
Cuong Pham <kuongfam31797@gmail.com>
2023-06-05 23:05:12 +0700

    Create UsefulSwift project

---
+++ UsefulSwift/UsefulSwift/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UsefulSwift
+//
+//  Created by Cuong Pham on 05/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
